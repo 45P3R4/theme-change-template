5bfc70f2823c6acfab48db4ec2556896ec9eb239
ksamoilova <langleybaka@gmail.com>
2024-12-24 19:55:28 +0300

    change cookie storage to local storage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let currentTheme = getCookie("data-theme");
+let currentTheme = localStorage.getItem("theme");
 
 document.firstElementChild.setAttribute("data-theme", currentTheme);
 
@@ -7,12 +7,5 @@ function changeTheme()
     currentTheme = currentTheme == "light" ? "dark" : "light";
 
     document.firstElementChild.setAttribute("data-theme", currentTheme);
-    document.cookie = "data-theme=" + currentTheme;
+    localStorage.setItem("theme", currentTheme);
 }
-
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
